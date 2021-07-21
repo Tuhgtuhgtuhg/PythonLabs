8cad7c501307258356de2b59e4fa021202ff713a
Tuhgtuhgtuhg <tuhgtuhgtuhg@gmail.com>
2021-07-21 23:01:56 +0300

    Added .gitignore file

--- lab07/lab07.py
+++ lab07/lab07.py
@@ -1,12 +1,11 @@
-from numpy import *
+import numpy 
 
-import math
 
 import matplotlib.pyplot as plt
-x = linspace(0, 5, 100)
+x = numpy.linspace(0, 5, 100)
 
 
-y = cos(x*x)/x
+y = numpy.cos(x*x)/x
 
 plt.plot(x, y, 'm-')
 plt.legend(['Y(x)=cos(x^2)/x, x=[0...5]'], loc='upper right')
